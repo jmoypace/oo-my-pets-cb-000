1e7cffe0bbdcf6cb74d0a81047e75f7ba95babda
jmoypace <jmoypace@gmail.com>
2018-07-17 20:17:41 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,7 +47,12 @@ def feed_fish
    end
  end
 def sell_pets
-
+  pets.each do |species, animals|
+       animals.each do |animal|
+         animal.mood = "nervous"
+       end
+       animals.clear
+     end
 end
 def list_pets
 
