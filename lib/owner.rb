dd9592524761e3c448f48fe81ef74879cfed348b
jmoypace <jmoypace@gmail.com>
2018-07-17 19:50:04 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,12 @@
 class Owner
   # code goes here
+  OWNERS=[]
+  attr_accessor :name, :pets
+  attr_reader :species
   @@all=[]
   @@counter=0
 def initialize(animal)
-  
+
   @@all<<self
   @@counter+=1
 end
