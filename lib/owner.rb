add6f2a835fae448a1829de502995eb33c902bac
jmoypace <jmoypace@gmail.com>
2018-07-17 19:42:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,18 @@
 class Owner
   # code goes here
+  @@all=[]
+  @@counter=0
+def initialize
+  @@all<<self
+  @@counter+=1
+end
+  def self.all
+    @@all
+  end
+  def count
+    @@counter
+  end
+  def reset
+    @@all.clear
+  end
 end
